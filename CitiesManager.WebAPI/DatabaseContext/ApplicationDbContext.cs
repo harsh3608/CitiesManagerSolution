305fd2bc0d9aee87b61dddd3ee4e78bd5e952872
Harsh Patel <harsh3608@gmail.com>
2023-05-16 12:22:05 +0530

    created city controller

--- CitiesManager.WebAPI/DatabaseContext/ApplicationDbContext.cs
+++ CitiesManager.WebAPI/DatabaseContext/ApplicationDbContext.cs
@@ -21,12 +21,12 @@ namespace CitiesManager.WebAPI.DatabaseContext
 
             modelBuilder.Entity<City>().HasData(new City() 
             {
-                CityId = Guid.Parse("5AC5FB53-F5D1-4FC4-AABF-4E1B4E2A4AC8"),
+                CityID = Guid.Parse("5AC5FB53-F5D1-4FC4-AABF-4E1B4E2A4AC8"),
                 CityName = "Bhopal"
             });
             modelBuilder.Entity<City>().HasData(new City()
             {
-                CityId = Guid.Parse("19FBA6C3-829E-4968-A7E3-87DB6431E31C"),
+                CityID = Guid.Parse("19FBA6C3-829E-4968-A7E3-87DB6431E31C"),
                 CityName = "Ahmedabad"
             });
         }
--- CitiesManager.WebAPI/Models/City.cs
+++ CitiesManager.WebAPI/Models/City.cs
@@ -5,7 +5,7 @@ namespace CitiesManager.WebAPI.Models
     public class City
     {
         [Key]
-        public Guid CityId { get; set; }
-        public string CityName { get; set; }
+        public Guid CityID { get; set; }
+        public string? CityName { get; set; }
     }
 }
